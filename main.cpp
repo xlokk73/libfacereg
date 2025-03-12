e8c3871d7d030cc3c5e7327f0810944630cef479
manwel <manwelbugeja23@icloud.com>
2025-03-12 15:37:24 +0100

    Merged projects

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "FaceLib.h"  // Update the path if necessary
+
+int main() {
+    std::string S = "Test 123456";
+
+    StringLength(S);
+    ReverseString(S);
+
+    return 0;
+}
