cf764f32289fda629a236178f619cc92e4bcc4f3
GabeV28 <gabevassallo28@gmail.com>
2025-06-23 12:54:48 +0200

    Commit 23/06

--- FaceLib.cpp
+++ FaceLib.cpp
@@ -1,12 +1,64 @@
+#include "FaceLib.h"
+#include <opencv2/opencv.hpp>
 #include <algorithm>
-#include <string>
 #include <iostream>
 
-void ReverseString(std::string& s) {
-    std::reverse(s.begin(), s.end());
-    std::cout << "Reversed string: " << s << std::endl;
+// String utility functions
+FACELIB_API int StringLength(std::string& str) {
+    return static_cast<int>(str.length());
 }
 
-void StringLength(const std::string& s) {
-    std::cout << "Length of string: " << s.length() << std::endl;
+FACELIB_API void ReverseString(std::string& str) {
+    std::reverse(str.begin(), str.end());
+}
+
+FACELIB_API void Test() {
+    std::string version = cv::getVersionString();
+    std::cout << "OpenCV Version: " << version << std::endl;
+}
+
+// Image processing functions
+FACELIB_API cv::Mat loadImage(const std::string& filename) {
+    cv::Mat image = cv::imread(filename);
+    if (image.empty()) {
+        std::cerr << "Error: Could not load image: " << filename << std::endl;
+    }
+    return image;
+}
+
+FACELIB_API cv::Mat loadImageGrayscale(const std::string& filename) {
+    cv::Mat image = cv::imread(filename, cv::IMREAD_GRAYSCALE);
+    if (image.empty()) {
+        std::cerr << "Error: Could not load grayscale image: " << filename << std::endl;
+    }
+    return image;
+}
+
+FACELIB_API void displayImage(const cv::Mat& image, const std::string& windowName, int waitTime) {
+    if (image.empty()) {
+        std::cerr << "Error: Cannot display empty image" << std::endl;
+        return;
+    }
+
+    cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
+    cv::imshow(windowName, image);
+
+    // Only wait if waitTime > 0
+    if (waitTime > 0) {
+        cv::waitKey(waitTime);
+    }
+    // Don't wait if waitTime is 0 - let caller handle it
+}
+
+FACELIB_API void closeAllWindows() {
+    cv::destroyAllWindows();
+}
+
+// Implementation of rotate90
+FACELIB_API void rotate90(cv::Mat& my_image) {
+    if (my_image.empty()) {
+        std::cerr << "Error: Cannot rotate an empty image." << std::endl;
+        return;
+    }
+    cv::rotate(my_image, my_image, cv::ROTATE_90_CLOCKWISE);
 }
--- FaceLib.h
+++ FaceLib.h
@@ -2,8 +2,31 @@
 #define FACELIB_H
 
 #include <string>
+#include <opencv2/opencv.hpp>
 
-void ReverseString(std::string& s);
-size_t StringLength(const std::string& s);
+// Define export macro properly
+#ifdef _WIN32
+    #ifdef FACELIB_EXPORTS
+        #define FACELIB_API __declspec(dllexport)
+    #else
+        #define FACELIB_API __declspec(dllimport)
+    #endif
+#else
+    #define FACELIB_API
+#endif
+
+// String utility functions
+FACELIB_API int StringLength(std::string& str);
+FACELIB_API void ReverseString(std::string& str);
+FACELIB_API void Test();
+
+// Image processing functions
+FACELIB_API cv::Mat loadImage(const std::string& filename);
+FACELIB_API cv::Mat loadImageGrayscale(const std::string& filename);
+FACELIB_API void displayImage(const cv::Mat& image, const std::string& windowName, int waitTime = 0);
+FACELIB_API void closeAllWindows();
+
+// Image manipulation functions
+FACELIB_API void rotate90(cv::Mat& my_image);
 
 #endif // FACELIB_H
